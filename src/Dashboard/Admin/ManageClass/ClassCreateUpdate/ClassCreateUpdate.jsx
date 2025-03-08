a84227b2e5b3a85066f41082556deb9e8268c962
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-03-08 16:23:21 +0600

    added calass page

--- src/Dashboard/Admin/ManageClass/ClassCreateUpdate/ClassCreateUpdate.jsx
+++ src/Dashboard/Admin/ManageClass/ClassCreateUpdate/ClassCreateUpdate.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import { IoMdInformationCircleOutline } from "react-icons/io";
 import InputField from "../../../../Component/InputField/InputField";
+import SelectInputField from "../../../../Component/SelectInputField/SelectInputField";
 
-const SectionCreateUpdate = ({ create, setCurrentPage }) => {
+const ClassCreateUpdate = ({ create, setCurrentPage }) => {
   return (
     <div>
       {/* =====> Tab Part Start <===== */}
       <div className="flex items-center justify-between pt-[10px] pb-[20px]">
         <div>
           <h2 className="text-[20px] font-semibold text-[#202C4B]">
-            {create ? "  Update Teacher" : "  Add Teacher"}
+            {create ? "  Update Class" : "  Add Class"}
           </h2>
           <div className="flex items-center gap-[10px]">
             <p className="text-[#6A7287] text-[14px]">Dashboard</p>
             <p className="text-[#6a72876c]">/</p>
             <p className="text-[#6A7287] text-[14px]">Admin</p>
             <p className="text-[#6a72876c]">/</p>
-            <p className="text-[#6A7287] text-[14px]">Add Section</p>
+            <p className="text-[#6A7287] text-[14px]">Add Class</p>
           </div>
         </div>
       </div>
@@ -25,27 +26,34 @@ const SectionCreateUpdate = ({ create, setCurrentPage }) => {
           <div className="bg-white p-[5px] rounded-[5px]">
             <IoMdInformationCircleOutline className="text-[#202C4B] text-[14px]" />
           </div>
-          <h2 className="text-[#202C4B] font-semibold text-[18px]">Section</h2>
+          <h2 className="text-[#202C4B] font-semibold text-[18px]">Class</h2>
         </div>
 
         {/*  */}
-        <div className="p-[20px] ">
-          <InputField title={"Section Name"} />
+        <div className="p-[20px] grid grid-cols-1 lg:grid-cols-2 gap-[20px]">
+          <SelectInputField
+            title={"Section Name"}
+            options={[
+              { id: 1, value: "নতুন" },
+              { id: 2, value: "পুরাতন" },
+            ]}
+          />
+          <InputField title={"Class Name"} />
         </div>
 
         {/*  */}
         <div className="flex items-end justify-end gap-[10px] mb-[15px]">
           <button
-            onClick={() => setCurrentPage("sectionList")}
+            onClick={() => setCurrentPage("classList")}
             className="text-[14px] font-semibold text-[#202c4bab] hover:bg-red-500 hover:text-white py-[8px] px-[15px] bg-gray-200 rounded-[5px]  border"
           >
             Cancle
           </button>
           <button
-            onClick={() => setCurrentPage("sectionList")}
+            onClick={() => setCurrentPage("classList")}
             className="text-[14px] font-semibold text-[#fff] bg-[#506EE4] py-[8px] px-[15px] rounded-[5px]  border"
           >
-            Add Teacher
+            Add Class
           </button>
         </div>
       </div>
@@ -53,4 +61,4 @@ const SectionCreateUpdate = ({ create, setCurrentPage }) => {
   );
 };
 
-export default SectionCreateUpdate;
+export default ClassCreateUpdate;
--- src/Dashboard/Admin/ManageClass/ManageClass.jsx
+++ src/Dashboard/Admin/ManageClass/ManageClass.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import ClassCreateUpdate from "./ClassCreateUpdate/ClassCreateUpdate";
+import ClassListComponent from "./ClassListComponent/ClassListComponent";
 
 const ManageClass = () => {
-  return <div>class</div>;
+  const [currentPage, setCurrentPage] = useState("classList");
+  const [create, setCreate] = useState(false);
+
+  const handleCreateUpdate = (value) => {
+    if (value == "classUpdate") {
+      setCreate(true);
+      setCurrentPage("classCreateUpdate");
+    }
+    if (value == "classCreate") {
+      setCurrentPage("classCreateUpdate");
+    }
+  };
+
+  return (
+    <div>
+      {currentPage === "classList" ? (
+        <ClassListComponent
+          setCurrentPage={setCurrentPage}
+          handleCreateUpdate={handleCreateUpdate}
+        />
+      ) : currentPage === "classCreateUpdate" ? (
+        <ClassCreateUpdate create={create} setCurrentPage={setCurrentPage} />
+      ) : null}
+    </div>
+  );
 };
 
 export default ManageClass;
