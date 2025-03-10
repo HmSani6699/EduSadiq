8f12d09e1e499ff651a6ea37b6db83b956e428e4
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-03-10 10:06:00 +0600

    complete responsive feescollect page

--- src/Component/FeesCollect/FeesCollect.jsx
+++ src/Component/FeesCollect/FeesCollect.jsx
@@ -7,8 +7,8 @@ import TextareaFiled from "../InputField/TextareaFiled";
 
 const FeesCollect = ({ setFeesCollectOpen }) => {
   return (
-    <div className="fixed inset-0 z-[70] flex justify-center  py-[30px]  bg-black bg-opacity-50">
-      <div className="bg-white  rounded-lg shadow-lg relative lg:w-[800px]  feesCollect overflow-x-auto">
+    <div className="fixed inset-0 z-[70] flex justify-center  lg:py-[30px] p-[10px] bg-black bg-opacity-50">
+      <div className="bg-white  rounded-lg shadow-lg relative lg:w-[800px] w-full feesCollect overflow-x-auto">
         <div className="flex items-center justify-between border-b p-[15px]">
           <div className="flex items-center gap-[10px]">
             <h2 className="text-[20px] text-[#202C4B] font-bold">
@@ -24,7 +24,7 @@ const FeesCollect = ({ setFeesCollectOpen }) => {
         </div>
 
         {/*  */}
-        <div className="flex items-center justify-between bg-[#f2f5ffa9] mx-[20px] p-[20px] rounded-[5px] my-[20px]">
+        <div className="lg:flex items-center justify-between bg-[#f2f5ffa9] mx-[20px] p-[20px] rounded-[5px] my-[20px]">
           <div className="flex items-center gap-[10px]">
             <div className="w-[50px] h-[50px] rounded-[5px]">
               <img className="rounded-[5px]" src={user} alt="user jpg" />
@@ -35,12 +35,12 @@ const FeesCollect = ({ setFeesCollectOpen }) => {
             </div>
           </div>
 
-          <div>
+          <div className="mt-[15px] lg:mt-0">
             <h2 className="text-[#202C4B] font-semibold">Total Outstanding</h2>
             <p className="text-[#202C4B] text-[14px]">1000</p>
           </div>
 
-          <button className="flex items-center bg-red-100 text-[14px] text-red-500 font-semibold py-[2px] px-[6px] rounded-[5px]">
+          <button className="flex items-center mt-[15px] lg:mt-0 bg-red-100 text-[14px] text-red-500 font-semibold py-[2px] px-[6px] rounded-[5px]">
             <GoDotFill />
             <p>Unpaid</p>
           </button>
