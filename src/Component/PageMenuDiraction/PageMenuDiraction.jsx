d0a6fa86a96170b69e503fbd9d1544ca812105cb
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-03-13 05:59:45 +0600

    added addendance list

--- src/Component/PageMenuDiraction/PageMenuDiraction.jsx
+++ src/Component/PageMenuDiraction/PageMenuDiraction.jsx
@@ -9,6 +9,7 @@ const PageMenuDiraction = ({
   currentPage,
   setCurrentPage,
   buttonTitle,
+  addButton,
 }) => {
   return (
     <div className="lg:flex items-center justify-between pt-[10px] pb-[20px]">
@@ -30,12 +31,14 @@ const PageMenuDiraction = ({
         <button className="bg-[#E9EDF4] text-[14px] text-[#515B73] font-semibold flex items-center gap-[8px] rounded-[5px] py-[8px] px-[15px] shadow-sm">
           <img src={export_icon} alt="" /> Export CSV
         </button>
-        <button
-          onClick={() => setCurrentPage(false)}
-          className="bg-[#506EE4] text-[14px] text-white font-semibold flex items-center gap-[8px] rounded-[5px] py-[8px] px-[15px] shadow-sm"
-        >
-          <FaPlus className="text-[12px]" /> {buttonTitle}
-        </button>
+        {addButton && (
+          <button
+            onClick={() => setCurrentPage(false)}
+            className="bg-[#506EE4] text-[14px] text-white font-semibold flex items-center gap-[8px] rounded-[5px] py-[8px] px-[15px] shadow-sm"
+          >
+            <FaPlus className="text-[12px]" /> {buttonTitle}
+          </button>
+        )}
       </div>
     </div>
   );
